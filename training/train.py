c27e20202007cd868b61bb1aa92a89c9a0cce0a6
Emanuele Harrasser <emanuele.harrasser@stud.tu-darmstadt.de>
2026-05-09 17:41:36 +0200

    added path to train

--- training/train.py
+++ training/train.py
@@ -4,6 +4,11 @@ from pathlib import Path
 import argparse
 import csv
 import random
+import sys
+from pathlib import Path as _Path
+
+sys.path.insert(0, str(_Path(__file__).resolve().parents[1]))
+
 
 import torch
 from torch import nn
